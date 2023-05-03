2387839ef2f4cf481e9781dd3325128738253eec
Luka Čulina <luka.culina56@gmail.com>
2023-05-03 11:26:13 +0200

    setup gh-pages

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+import MovieCard from './MovieCard';
+import './App.css';
+import SearchIcon from './search.svg';
+
+const API_URL = 'http://www.omdbapi.com?apikey=80c970f0';
+
+const App = () => {
+
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const searchMovies = async(title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+    }
+
+    const EnterKeyPress = (event) => {
+        if (event.key === "Enter") {
+            searchMovies(searchTerm);
+        }
+      };    
+
+    useEffect(() =>{
+        searchMovies('Barbie');
+    }, [])
+
+    return (
+        <div className='app'>
+            <h1>MovieBay</h1>
+            <div className='search'>
+                <input type="text" 
+                    placeholder='Search for movies' 
+                    value={searchTerm}
+                    onChange={(e)=>setSearchTerm(e.target.value)}
+                    onKeyDown={EnterKeyPress}
+                />
+                <img src={SearchIcon} 
+                    alt="search" 
+                    onClick={()=>searchMovies(searchTerm)}
+                />
+            </div>
+            {movies?.length > 0
+            ?(
+                <div className='container'>
+                    {movies.map((movie)=>(
+                        <MovieCard movie ={movie}/>
+                    ))}
+                </div>
+            ) : (
+                <div className='empty'>
+                    <h2>No movies found</h2>
+                </div>
+            )}
+    </div>        
+    );
+}
+
+export default App;
